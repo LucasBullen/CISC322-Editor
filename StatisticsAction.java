604ab5b45e05c41860b419509d027c26d893f8b8
LucasBullen <bullenlucas@gmail.com>
2016-11-23 18:29:12 -0500

    statistics

--- StatisticsAction.java
+++ StatisticsAction.java
@@ -1,3 +1,6 @@
+import javax.swing.JOptionPane;
+import javax.swing.table.*;
+
 class StatisticsAction extends CSVAction {
 	public StatisticsAction() {
 		super("Statistics");
@@ -13,6 +16,50 @@ class StatisticsAction extends CSVAction {
      */
     protected void changeCSV(CSVContents con, int row, int col) {
 		System.out.println("changeText:"+row+","+col);
-        con.dftTbl.setValueAt("cheese", row, col);
+        DefaultTableModel table = con.dftTbl;
+        int rows = table.getRowCount();
+        float[] rowData = new float[rows];
+        int sum = 0;
+        int nonblank = 0;
+        float max = 0;
+        float min = 0;
+        boolean maxMinSet = false;
+
+        for (int i = 0; i < rows; i++) {
+            String valString = (String)table.getValueAt(i,col);
+            if(valString == null || valString.isEmpty()){
+                continue;
+            }else if(!isNumeric(valString)){
+                JOptionPane.showMessageDialog(null, "Error: non-numeric value in row.");
+                return;
+            }
+            float val = Float.parseFloat(valString);
+            sum += val;
+            nonblank++;
+            if (!maxMinSet) {
+                max = val;
+                min = val;
+                maxMinSet = true;
+            }else if(max < val){
+                max = val;
+            }else if(min > val){
+                min = val;
+            }
+        }
+        float mean = sum / nonblank;
+        JOptionPane.showMessageDialog(null, "Statistics:"+
+            "\nSum: "+sum+
+            "\nElements: "+nonblank+
+            "\nMean: "+mean+
+            "\nMax: "+max+
+            "\nMin: "+min
+        );
+        con.dftTbl = table;
     } // end changeText
+
+    private boolean isNumeric(String str)
+    {
+      return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
 } // end class UpCaseAction
